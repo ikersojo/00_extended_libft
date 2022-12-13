125cb3f52323c31c41dd4c4f36c09cafe6cb537b
Iker Sojo <ikersojo@hotmail.com>
2022-12-13 20:36:28 +0100

    update

--- src/ft_terminal/ft_get_user_input.c
+++ src/ft_terminal/ft_get_user_input.c
@@ -6,7 +6,7 @@
 /*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 20:03:30 by isojo-go          #+#    #+#             */
-/*   Updated: 2022/12/10 20:10:15 by isojo-go         ###   ########.fr       */
+/*   Updated: 2022/12/13 20:28:01 by isojo-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,15 @@ char	*ft_get_user_input(char *str)
 
 	ft_putstr_fd(str, 1);
 	line = (char *)malloc(400);
+	if (!line)
+		return (NULL);
 	bytes = read(0, &c, 1);
 	i = 0;
 	while (bytes > 0 && c != '\0' && i < 395)
 	{
-		*(line + i++) = c;
 		if (c == '\n')
 			break ;
+		*(line + i++) = c;
 		bytes = read(0, &c, 1);
 	}
 	*(line + i) = '\0';
